8a17fe046b66501da098b03c637a28af062a5657
Martiks38 <cappa.giuliano@gmail.com>
2023-04-27 01:18:27 -0300

    Add getPetTypes controller

--- src/lib/controllers/petController.ts
+++ src/lib/controllers/petController.ts
@@ -1,13 +1,39 @@
+import { errorMessage } from '@/utils/handlerError'
 import PetModel from '../models/petModel'
-import type { ErrorResult, Result } from '@/typings/interfaces'
+import type { Result } from '@/typings/interfaces'
 
-export async function getPets(selfLink: string): Promise<Result> {
+export async function getPets(base: string, selfLink: string): Promise<Result> {
 	try {
 		const results = await PetModel.find({}, { _id: false })
 		const total = results.length
 
-		return { links: { self: selfLink }, results, total }
+		return {
+			links: {
+				base,
+				self: selfLink
+			},
+			results,
+			total
+		}
 	} catch (error) {
-		throw { status: 500, message: 'Internal Server Error' }
+		throw errorMessage(500)
+	}
+}
+
+export async function getPetTypes(base: string, selfLink: string): Promise<Result> {
+	try {
+		const results: string[] = await PetModel.distinct('type')
+		const total = results.length
+
+		return {
+			links: {
+				base,
+				self: selfLink
+			},
+			results,
+			total
+		}
+	} catch (error) {
+		throw errorMessage(500)
 	}
 }
