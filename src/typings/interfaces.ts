36def02f8bff1ae08f2ba67c2c3977154a9529f8
Martiks38 <cappa.giuliano@gmail.com>
2023-04-26 22:18:29 -0300

    Create API response interface

--- src/typings/interfaces.ts
+++ src/typings/interfaces.ts
@@ -1,9 +1,26 @@
-import type { PetType } from './types'
+import { PET_TYPES } from '@/consts'
+import type { PetTypes } from './types'
 
 export interface Pet {
 	name: string
 	description: string
 	price: number
 	media: string
-	type: PetType
+	type: PetTypes
+}
+
+export interface Result {
+	results?: Pet[] | typeof PET_TYPES /* Result of the request to the database */
+	page_number?: string /* Page number indicating the offset to display the request results */
+	total?: number /* Total possible elements for the request */
+	size?: number /* Total element of the response */
+	links?: {
+		base?: string /* Base url */
+		self?: string /* Request URL */
+		prev_page?: string /* URL of the previous page */
+		next_page?: string /* URL of the next page */
+		first?: string /* First page URL */
+		last?: string /* URL of the last page */
+	}
+	message?: string /*  */
 }
