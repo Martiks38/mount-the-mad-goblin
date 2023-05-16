a34ed5717df4d7dd0514b50d13c68b0c0c3f8b18
Martiks38 <cappa.giuliano@gmail.com>
2023-05-16 19:00:29 -0300

    Create SessionForm component and add logic to login.tsx

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,51 +1,103 @@
-import Image from 'next/image'
 import Link from 'next/link'
-import { useId, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useId, useState } from 'react'
+import { useCart } from '@/hooks/useCart'
 import { useInputfocus } from '@/hooks/useInputfocus'
+import { useUser } from '@/hooks/useUser'
 
-import img from '@/assets/imgs/heroImgHome.webp'
+import { SessionForm } from '@/common/SessionForm'
+
 import loginStyles from '@/styles/pages/LogIn_SignUp.module.css'
 
+interface IResponse {
+	ok: boolean
+	token: string
+}
+
 export default function LogIn() {
+	const router = useRouter()
 	const [isError, setIsError] = useState(false)
 	const userInputId = useId()
 	const passwordInputId = useId()
 	const keepLoggedInputId = useId()
 	const focusInput = useInputfocus(null)
+	const { setToken, setUsername } = useUser()
+	const { connected, setConnection } = useCart()
+
+	useEffect(() => {
+		if (connected) router.push('/')
+	}, [connected, router])
+
+	const handleLoginSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
+		ev.preventDefault()
+
+		const formData = new FormData(ev.currentTarget)
+
+		let urlSearchParams: Record<string, string> = {}
+
+		for (const pair of Array.from(formData.entries())) {
+			if (pair[0] === 'keepLogged') continue
+
+			urlSearchParams[pair[0]] = pair[1] as string
+		}
+
+		const body = new URLSearchParams(urlSearchParams).toString()
+
+		try {
+			const response = await fetch('http://localhost:3000/api/v1/users', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'x-www-form-urlencoded'
+				},
+				body
+			})
+
+			const data: IResponse = await response.json()
+
+			if (!data.ok) throw data
+			if (formData.get('keepLogged')) {
+				setUsername(formData.get('username') as string)
+				setToken(data.token)
+			}
+
+			setConnection(true)
+
+			router.push('/')
+		} catch (error) {
+			console.error(error)
+		}
+	}
 
 	return (
 		<section className={`content ${loginStyles.centerContent}`}>
-			<div className={loginStyles.containerForm}>
-				<Image src={img} alt="" className={loginStyles.containerForm__img} />
-				<form onSubmit={() => {}} className={loginStyles.containerForm__form}>
-					<h1 className={loginStyles.title}>Log In</h1>
-					{isError && (
-						<p className={loginStyles.containerForm__error} aria-live="polite">
-							The credentials you are using are not valid.
-						</p>
-					)}
-					<label htmlFor={userInputId}>Username *</label>
-					<input type="text" id={userInputId} name="username" required ref={focusInput} />
-					<label htmlFor={passwordInputId}>Password *</label>
-					<input type="password" id={passwordInputId} name="password" required />
-					<div className={loginStyles.containerForm__options}>
-						<label
-							htmlFor={keepLoggedInputId}
-							className={loginStyles.containerForm__options__wrapperCheckbox}
-						>
-							<div className={loginStyles.containerForm__options__wrapperCheckbox__input}>
-								<input type="checkbox" name="keepLogged" id={keepLoggedInputId} />
-								<div></div>
-							</div>
-							<span>Keep me logged in</span>
-						</label>
-						<Link href="#">Forgot username or password?</Link>
-					</div>
-					<button type="submit" className={loginStyles.containerForm__submit}>
-						Log in
-					</button>
-				</form>
-			</div>
+			<SessionForm submit={handleLoginSubmit}>
+				<h1 className={loginStyles.title}>Log In</h1>
+				{isError && (
+					<p className={loginStyles.containerForm__error} aria-live="polite">
+						The credentials you are using are not valid.
+					</p>
+				)}
+				<label htmlFor={userInputId}>Username *</label>
+				<input type="text" id={userInputId} name="username" required ref={focusInput} />
+				<label htmlFor={passwordInputId}>Password *</label>
+				<input type="password" id={passwordInputId} name="password" required />
+				<div className={loginStyles.containerForm__options}>
+					<label
+						htmlFor={keepLoggedInputId}
+						className={loginStyles.containerForm__options__wrapperCheckbox}
+					>
+						<div className={loginStyles.containerForm__options__wrapperCheckbox__input}>
+							<input type="checkbox" name="keepLogged" id={keepLoggedInputId} defaultChecked />
+							<div></div>
+						</div>
+						<span>Keep me logged in</span>
+					</label>
+					<Link href="#">Forgot username or password?</Link>
+				</div>
+				<button type="submit" className={loginStyles.containerForm__submit}>
+					Log in
+				</button>
+			</SessionForm>
 		</section>
 	)
 }
