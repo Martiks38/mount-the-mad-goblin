f9fb0b8db9d972ce6134ad7fc5faa8d22ac9fb54
Martiks38 <cappa.giuliano@gmail.com>
2023-05-26 20:16:42 -0300

    feat: add effect to load saved shopping cart
    Load the shopping cart saved in a cookie and update the store shopping list.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
+import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useCart } from '@/hooks/useCart'
 import { useUser } from '@/hooks/useUser'
+
 import { LayoutPage } from '@/layout/LayoutPage'
+
+import { shoppingCartCookie } from '@/consts'
 
 import '@/styles/globals.css'
 import '@/styles/reset.css'
 
-import type { AppProps } from 'next/app'
+import type { PurchasedPet } from '@/typings/interfaces'
 
 interface Response {
 	ok: boolean
@@ -15,10 +19,48 @@ interface Response {
 	token?: string
 }
 
+type PurchasedPetsCookie = {
+	purchasedPets: PurchasedPet[]
+}
+
 export default function App({ Component, pageProps }: AppProps) {
 	const { setConnection } = useCart()
 	const { token, username, setToken } = useUser()
+	const { setPurchasedPets } = useCart()
 
+	// In case there are previous unconfirmed purchases, update the shopping cart store.
+	useEffect(() => {
+		const cookies = document.cookie
+		let purchases: PurchasedPet[] | null = null
+		let purchaseCookie: string | string[] | undefined = undefined
+
+		if (cookies === '') return
+
+		if (cookies.includes(';')) {
+			let cookieValues: string[][]
+
+			cookieValues = cookies.split(';').map((cookie) => cookie.split('='))
+			purchaseCookie = cookieValues.find((cookie) => cookie[0] === shoppingCartCookie)
+
+			if (purchaseCookie) purchases = JSON.parse(purchaseCookie[1])
+		} else {
+			purchaseCookie = cookies.split('=')[1]
+		}
+
+		if (typeof purchaseCookie === 'string') {
+			const { purchasedPets } = JSON.parse(purchaseCookie as string) as PurchasedPetsCookie
+			purchases = purchasedPets
+		}
+
+		if (typeof purchaseCookie === 'object') {
+			const { purchasedPets } = JSON.parse(purchaseCookie[1]) as PurchasedPetsCookie
+			purchases = purchasedPets
+		}
+
+		if (purchases) setPurchasedPets(purchases)
+	}, [setPurchasedPets])
+
+	// Check if the user's session is kept open.
 	useEffect(() => {
 		const checkKeepSession = (token: string, username: string) => {
 			const body = new URLSearchParams({ token, username })
