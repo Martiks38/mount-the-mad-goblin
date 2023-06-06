43f84d81520564bafda6698bb10c07e854e10754
Martiks38 <cappa.giuliano@gmail.com>
2023-06-06 18:19:38 -0300

    refactor: add production url and development api

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
-export const { DB_DEV, DB_PROD, API_URL, API_URL_V1, SECRET_TOKEN } = process.env
+export const {
+	DB_DEV,
+	DB_PROD,
+	API_URL_DEV,
+	API_URL_V1_DEV,
+	API_URL_PROD,
+	API_URL_V1_PROD,
+	SECRET_TOKEN
+} = process.env
