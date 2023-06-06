34fe1a7b069963abf17f138de3a034b15108e422
Martiks38 <cappa.giuliano@gmail.com>
2023-06-06 18:26:11 -0300

    feat: create libraries of api resources for production and development

--- src/consts.ts
+++ src/consts.ts
@@ -31,3 +31,29 @@ export const INVALID_BILLING_INFORMATION = 'billing-information'
 export const LIMIT = 10
 
 export const shoppingCartCookie = 'shopping-cart'
+
+const API_RESOURCES_PROD = {
+	auth: 'https://pets-the-crazy-goblin.vercel.app/api/v1/auth',
+	pets: {
+		search: 'https://pets-the-crazy-goblin.vercel.app/api/v1/pets/search',
+		types: 'https://pets-the-crazy-goblin.vercel.app/api/v1/pets/types',
+		prices: 'https://pets-the-crazy-goblin.vercel.app/api/v1/pets/prices',
+		base: 'https://pets-the-crazy-goblin.vercel.app/api/v1/pets'
+	},
+	users: 'https://pets-the-crazy-goblin.vercel.app/api/v1/users'
+}
+
+const API_RESOURCES_DEV = {
+	auth: 'http://localhost:3000/api/v1/auth',
+	pets: {
+		search: 'http://localhost:3000/api/v1/pets/search',
+		types: 'http://localhost:3000/api/v1/pets/types',
+		prices: 'http://localhost:3000/api/v1/pets/prices',
+		base: 'http://localhost:3000/api/v1/pets'
+	},
+	users: 'http://localhost:3000/api/v1/users'
+}
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+export const apiURLs = isProduction ? API_RESOURCES_PROD : API_RESOURCES_DEV
